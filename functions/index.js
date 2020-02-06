b7412a5f859c4ec71f7b396dc042d85b8deeafd9
Boris Lukanovic <boris.lukanovic17@gmail.com>
2020-02-06 16:08:31 +0100

    express and format response

--- functions/index.js
+++ functions/index.js
@@ -7,38 +7,38 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://social-media-app-73241.firebaseio.com"
 })
+// admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
-})
+const express = require("express")
+const app = express()
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
+    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       let screams = []
       data.forEach(doc => {
-        screams.push(doc.data())
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        })
       })
       return res.json(screams)
     })
     .catch(err => console.error(err))
 })
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: "Method not allowed" })
-  }
-
+// eslint-disable-next-line consistent-return
+app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   }
 
   admin
@@ -54,3 +54,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err)
     })
 })
+
+exports.api = functions.https.onRequest(app)
