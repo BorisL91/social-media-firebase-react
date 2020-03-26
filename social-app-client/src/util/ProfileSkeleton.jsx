562e5954d467e64fb7695a6716d2a04d7af6624e
Boris <boris.lukanovic17@gmail.com>
2020-03-26 22:45:42 +0100

    styles

--- social-app-client/src/util/ProfileSkeleton.jsx
+++ social-app-client/src/util/ProfileSkeleton.jsx
@@ -10,72 +10,7 @@ import LocationOn from "@material-ui/icons/LocationOn"
 import LinkIcon from "@material-ui/icons/Link"
 import CalendarToday from "@material-ui/icons/CalendarToday"
 
-const styles = {
-  profile: {
-    "& .image-wrapper": {
-      textAlign: "center",
-      position: "relative",
-      "& button": {
-        position: "absolute",
-        top: "80%",
-        left: "70%"
-      }
-    },
-    "& .profile-image": {
-      width: 200,
-      height: 200,
-      objectFit: "cover",
-      maxWidth: "100%",
-      borderRadius: "50%"
-    },
-    "& .profile-details": {
-      textAlign: "center",
-      "& span, svg": {
-        verticalAlign: "middle"
-      },
-      "& a": {
-        color: "#00bcd4"
-      }
-    },
-    "& hr": {
-      border: "none",
-      margin: "0 0 10px 0"
-    },
-    "& svg.button": {
-      "&:hover": {
-        cursor: "pointer"
-      }
-    }
-  },
-  paper: {
-    padding: 20
-  },
-  handle: {
-    height: 20,
-    backgroundColor: "#00bcd4",
-    width: 60,
-    margin: "0 auto 7px auto"
-  },
-  fullLine: {
-    height: 15,
-    backgroundColor: "rgba(0,0,0,0.6)",
-    width: "100%",
-    marginBottom: 10
-  },
-  halfLine: {
-    height: 15,
-    backgroundColor: "rgba(0,0,0,0.6)",
-    width: "50%",
-    marginBottom: 10
-  },
-  profileImage: {
-    width: 200,
-    height: 200,
-    borderRadius: "50%",
-    objectFit: "cover",
-    maxWidth: "100%"
-  }
-}
+const styles = theme => themeStyles
 
 const ProfileSkeleton = props => {
   const { classes } = props
